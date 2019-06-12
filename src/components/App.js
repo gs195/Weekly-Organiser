90df1488c9cdfc2608f2bae9e6c2aa84efd1d35d
gs195 <timedilation01@gmail.com>
2019-06-12 19:51:11 +0800

    save6-functional

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import generateID from "./id-generator.js";
 import { Item, Input, Button } from "./functions-group-a";
 // import selectTaskList from "./task-list-selector";
 
+const days = { Monday: "Mon", Tuesday: "Tue" };
+
 class ToDoList extends React.Component {
   constructor(props) {
     super(props);
@@ -11,18 +13,14 @@ class ToDoList extends React.Component {
     this.state = {
       lastUpdated: "",
       value: "",
-      tasksMon: [
-        { id: 1, text: "buy milk", isDone: false, day: "Mon" },
-        { id: 2, text: "eat dinner", isDone: false, day: "Mon" },
-        { id: 3, text: "nail javascript", isDone: false, day: "Mon" },
-        { id: 4, text: "give feedback", isDone: false, day: "Mon" },
-        { id: 5, text: "find nemo", isDone: false, day: "Mon" }
-      ],
-      tasksTue: [{ id: 6, text: "find my keys", isDone: false, day: "Tue" }],
-      tasksWed: [],
-      tasksThur: [],
-      tasksFri: [],
-      tasksWknd: []
+      tasks: [
+        { id: 1, text: "buy milk", isDone: false, day: days.Monday },
+        { id: 2, text: "eat dinner", isDone: false, day: days.Monday },
+        { id: 3, text: "nail javascript", isDone: false, day: days.Monday },
+        { id: 4, text: "give feedback", isDone: false, day: days.Monday },
+        { id: 5, text: "find nemo", isDone: false, day: days.Monday },
+        { id: 6, text: "find my keys", isDone: false, day: days.Tuesday }
+      ]
     };
     this.handleEnterPress = this.handleEnterPress.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -30,11 +28,11 @@ class ToDoList extends React.Component {
   }
 
   tasksPending = () => {
-    const completedTasks = this.state.tasksMon.filter(t => {
+    const completedTasks = this.state.tasks.filter(t => {
       return t.isDone;
     });
 
-    return this.state.tasksMon.length - completedTasks.length;
+    return this.state.tasks.length - completedTasks.length;
   };
 
   handleStrikethrough(event) {
@@ -43,7 +41,7 @@ class ToDoList extends React.Component {
 
     this.setState(prevState => ({
       //synthetic event gets nullified in setState()
-      tasksMon: prevState.tasksMon.map(obj =>
+      tasks: prevState.tasks.map(obj =>
         obj.id === targetId
           ? Object.assign(
               obj,
@@ -59,12 +57,12 @@ class ToDoList extends React.Component {
   };
 
   taskListUpdate = () => {
-    // an onject literal is being directly appended to the tasksMon list
+    // an onject literal is being directly appended to the tasks list
     //in this.state because setState is being called immeiatedly after.
     //Otherwise, the components would not be re-rendered and setState
     //would need to be called in appending the object literal, which would
     //be done by setting a new modified list to this.state.tasts.
-    this.state.tasksMon.push({
+    this.state.tasks.push({
       id: generateID(),
       text: this.state.value,
       isDone: false
@@ -93,21 +91,19 @@ class ToDoList extends React.Component {
     }
   }
 
-  selectTaskList(listid) {
-    console.log("input tasklist selector day is: ", listid);
-    switch (listid) {
-      case "Mon":
-        console.log("returning this.state.tasksMon");
-        return this.state.tasksMon;
-      case "Tue":
-        console.log("returning this.state.tasksTue");
-        return this.state.tasksTue;
-      default:
-        throw new Error(
-          "input parameter to selectTaskList function is not one of the days"
-        );
-    }
-  }
+  // selectTaskList(listid) {
+  //   console.log("input tasklist selector day is: ", listid);
+  //   switch (listid) {
+  //     case "Mon":
+  //       return this.state.tasks;
+  //     case "Tue":
+  //       return this.state.tasksTue;
+  //     default:
+  //       throw new Error(
+  //         "input parameter to selectTaskList function is not one of the days"
+  //       );
+  //   }
+  // }
 
   dragOverHandler = event => {
     event.preventDefault();
@@ -115,112 +111,151 @@ class ToDoList extends React.Component {
 
   onDragStartHandler = (event, id, itemDay) => {
     console.log("id is ", id);
-    // for (let key in this.state) {
-    //   console.log("key is", key);
-    //   for (let i = 0; i < this.state[key].length; i++) {
-    //     console.log("this.state[key][i] and i are", this.state[key][i], i);
-    //     if (this.state[key][i] === event.target.day) {
-    //       var itemDay = this.state[key][i].day;
-    //       break;
-    //     }
-    //   }
-    // }
     console.log("itemDay is ", itemDay);
-    this.itemBeingDragged = this.selectTaskList(itemDay).filter(t => {
-      return t.id === Number(id);
-    });
-    console.log("object being dragged is ", this.itemBeingDragged);
-    // event.dataTransfer.setData("text/plain", event.target.key);
+    event.dataTransfer.setData("id", id);
+    // this.itemBeingDragged = this.state.tasks.filter(t => {
+    //   return t.id === Number(id);
+    // });
+    // console.log("object being dragged is ", this.itemBeingDragged);
   };
 
   onDropHandler = (event, imposition, itemDay, id) => {
-    //imposition is day of new list, itemDay is day of old list
 
-    //identify the taskList depending on source item's day attribute
-    let newList = this.selectTaskList(itemDay);
-    console.log("original target list is(found to be updated to reduced)", newList);
+    console.log("imposition is", imposition);
+    let idty = event.dataTransfer.getData("id");
 
-    //replace the object's day attribute with the hard-coded one in Item TaskList
-    let draggedObject = this.itemBeingDragged[0];
-    console.log("object that is being dragged is ", draggedObject);
-    draggedObject.day = imposition;
-    console.log("object that is being added is ", draggedObject);
-
-    //append dragged item to target taskList
-    newList.push(draggedObject);
-    console.log("The compounded target list is ", newList);
-
-    //delete dragged item from source taskList
-    console.log("original source list is", this.selectTaskList(itemDay));
-    let sourceList = this.selectTaskList(itemDay).filter(t => {
-      return t.id !== Number(id);
+    let d = this.state.tasks.filter(task => {
+      if (task.id == idty) {
+        task.day = imposition;
+      }
+      return task;
     });
-    console.log("The reduced source list is ", sourceList);
 
-    //update state
-    const myKeyTarget = this.getKeyByValue(
-      this.state,
-      this.selectTaskList(imposition)
-    );
-    // console.log("myKeyTarget is ", myKeyTarget);
-    const myKeySource = this.getKeyByValue(
-      this.state,
-      this.selectTaskList(itemDay)
-    );
+    console.log("d", d);
 
-    this.state.myKeyTarget = newList;
-    this.state.myKeySource = sourceList;
-    let timeNow = Date.now();
-    this.setState({ lastUpdated: timeNow });
-    // console.log("myKeySource is ", myKeySource);
-    // this.setState({ myKeyTarget: newList });
-    // this.setState({ myKeySource: sourceList });
+    this.setState({
+      ...this.state,
+      d
+    });
+
+    // imposition is day of new list, itemDay is day of old list
+    // console.log("!imposition is ", imposition);
+    // console.log("!itemDay is ", itemDay);
+    // console.log("id is ", id);
+    // //identify the taskList depending on source item's day attribute
+    // let a = [...this.state.tasks];
+    // let b = a.filter(t => {
+    //   return t.id !== this.itemBeingDragged[0].id;
+    // });
+    // console.log("b", b);
+
+    // let c = this.itemBeingDragged[0];
+    // c.id = imposition;
+
+    // console.log("c is", c);
+
+    // let newList = this.state.tasks.filter(t => {
+    //   return t.day === imposition;
+    // });
+    // console.log(
+    //   "original target list is",
+    //   newList
+    // );
+
+    // //replace the object's day attribute with the hard-coded one in Item TaskList
+    // let draggedObject = this.itemBeingDragged[0];
+    // console.log("object that is being dragged is ", draggedObject);
+    // draggedObject.day = imposition;
+    // console.log("object that is being added is ", draggedObject);
+
+    // //append dragged item to target taskList
+    // newList.push(draggedObject);
+    // console.log("The compounded target list is ", newList);
+
+    // //delete dragged item from source taskList
+    // let oldList = this.state.tasks.filter(t => {
+    //   return t.day === itemDay;
+    // });
+    // console.log(
+    //   "original src list is",
+    //   oldList
+    // );
+
+    // oldList = this.state.tasks.filter(t => {
+    //   return t.id !== Number(id);
+    // });
+    // console.log("The reduced src list is ", oldList);
+
+    // //update state
+    // const myKeyTarget = this.getKeyByValue(
+    //   this.state,
+    //   this.selectTaskList(imposition)
+    // );
+    // // console.log("myKeyTarget is ", myKeyTarget);
+    // const myKeySource = this.getKeyByValue(
+    //   this.state,
+    //   this.selectTaskList(itemDay)
+    // );
+
+    // this.state.myKeyTarget = newList;
+    // this.state.myKeySource = sourceList;
+    // let timeNow = Date.now();
+    // this.setState({ lastUpdated: timeNow });
+    // // console.log("myKeySource is ", myKeySource);
+    // // this.setState({ myKeyTarget: newList });
+    // // this.setState({ myKeySource: sourceList });
   };
 
   render() {
-    const TaskList = this.state.tasksMon.map(task => {
-      // const dayVar = 1;
-      return (
-        <Item
-          // day={dayVar}
-          day={task.day}
-          id={task.id}
-          key={generateID()}
-          description={task.text}
-          onClick={this.handleStrikethrough}
-          className={task.isDone ? "done" : "toDo"}
-          draggable="true"
-          onDragStart={event => {
-            console.log("this is being called");
-            this.onDragStartHandler(event, task.id, task.day);
-          }}
-          onDragOver={this.dragOverHandler}
-          onDrop={event => this.onDropHandler(event, "Mon", task.day, task.id)}
-        />
-      );
-    });
+    const TaskList = this.state.tasks
+      .filter(obj => obj.day === days.Monday)
+      .map(task => {
+        return (
+          <Item
+            day={task.day}
+            id={task.id}
+            key={generateID()}
+            description={task.text}
+            onClick={this.handleStrikethrough}
+            className={task.isDone ? "done" : "toDo"}
+            draggable="true"
+            onDragStart={event => {
+              console.log("this is being called");
+              this.onDragStartHandler(event, task.id, task.day);
+            }}
+            onDragOver={this.dragOverHandler}
+            onDrop={event =>
+              this.onDropHandler(event, days.Monday, task.day, task.id)
+            }
+          />
+        );
+      });
 
-    const TaskList2 = this.state.tasksTue.map(task => {
-      // const dayVar = 2;
-      return (
-        <Item
-          // day={dayVar}
-          day={task.day}
-          id={task.id}
-          key={generateID()}
-          description={task.text}
-          onClick={this.handleStrikethrough}
-          className={task.isDone ? "done" : "toDo"}
-          draggable="true"
-          onDragStart={event => {
-            console.log("tue is being called");
-            this.onDragStartHandler(event, task.id, task.day);
-          }}
-          onDragOver={this.dragOverHandler}
-          onDrop={event => this.onDropHandler(event, "Tue", task.day, task.id)}
-        />
-      );
-    });
+    const TaskList2 = this.state.tasks
+      .filter(obj => obj.day === days.Tuesday)
+      .map(task => {
+        // const dayVar = 2;
+        return (
+          <Item
+            // day={dayVar}
+            day={task.day}
+            id={task.id}
+            key={generateID()}
+            description={task.text}
+            onClick={this.handleStrikethrough}
+            className={task.isDone ? "done" : "toDo"}
+            draggable="true"
+            onDragStart={event => {
+              console.log("tue is being called");
+              this.onDragStartHandler(event, task.id, task.day);
+            }}
+            onDragOver={this.dragOverHandler}
+            onDrop={event =>
+              this.onDropHandler(event, days.Tuesday, task.day, task.id)
+            }
+          />
+        );
+      });
 
     //draggable -> to make the element in question a draggable component
     //onDrag -> an event listener that is triggered when the graggable element is
